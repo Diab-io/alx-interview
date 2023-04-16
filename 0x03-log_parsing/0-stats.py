8e73abd8dde92c58af9693f368abee1f6e1ac5ee
Odimayo David <odimayodavid7@gmail.com>
2023-04-16 19:30:05 +0100

    Updated file

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -5,41 +5,44 @@ module contains a script that reads stdin line by line and computes metrics
 import sys
 
 
-http_codes = {
-    "200": 0, "301": 0,
-    "400": 0, "401": 0,
-    "403": 0, "404": 0,
-    "405": 0, "500": 0
+status_codes = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0
 }
 
-total_size = 0
+file_size = 0
 
 
-def show_information():
+def print_metrics():
     """prints of the logs"""
-    print("File size: {}".format(total_size))
-    for code in sorted(http_codes.keys()):
-        if http_codes[code]:
-            print(f"{code}: {http_codes[code]}")
+    print("File size: {}".format(file_size))
+    for status in sorted(status_codes.keys()):
+        if status_codes[status]:
+            print("{}: {}".format(status, status_codes[status]))
 
 
 if __name__ == "__main__":
-    count = 1
+    count = 0
     try:
         for line in sys.stdin:
             try:
-                info = line.split()
-                size, code = info[-1], info[-2]
-                total_size += int(size)
-                if code in http_codes:
-                    http_codes[code] += 1
+                elems = line.split()
+                file_size += int(elems[-1])
+                if elems[-2] in status_codes:
+                    status_codes[elems[-2]] += 1
             except Exception:
                 pass
-            if count == 10:
-                show_information()
-                count = 0
+            if count == 9:
+                print_metrics()
+                count = -1
             count += 1
     except KeyboardInterrupt:
-        show_information()
+        print_metrics()
         raise
-    show_information()
+    print_metrics()
